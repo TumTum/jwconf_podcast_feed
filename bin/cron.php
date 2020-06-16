98748fc618f2b2a46b14d28d2ba452318422f47a
Tobias Matthaiou <tm@loberon.de>
2020-06-16 10:05:40 +0200

    Startet den CronJobs nur an diese bestimmte tagen.

--- bin/cron.php
+++ bin/cron.php
@@ -6,6 +6,12 @@
  * Time: 11:19
  */
 
+namespace tm\rss;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-(new \tm\rss\Crawler())->run();
+$today = date_create('now')->format('d.m.Y');
+
+if (array_search($today, Config::$events) !== false || Config::isDevMode()) {
+    (new \tm\rss\Crawler())->run();
+}
--- src/Config.php
+++ src/Config.php
@@ -8,13 +8,45 @@
 
 namespace tm\rss;
 
-
+/**
+ * Class Config
+ * @package tm\rss
+ */
 class Config
 {
+    /**
+     * An diesen tagen wird Kontrolliert ob neue Sendungen da sind
+     *
+     * @var string[]
+     */
+    public static $events = [
+        '12.07.2020',
+        '16.08.2020',
+        '13.09.2020',
+        '25.10.2020',
+        '15.11.2020',
+        '20.12.2020',
+    ];
+
+    /**
+     * Der download host.
+     *
+     * @var string
+     */
     public static $jwconf_host = "https://jwconf.org/";
 
+    /**
+     * Auf dieser Seite sind alle MP3 gelistet
+     *
+     * @var string
+     */
     public static $jwconf_url = "https://jwconf.org/sendungen/";
 
+    /**
+     * Spcher ort der Datenbank
+     *
+     * @var string
+     */
     public static $database_path = __DIR__ . '/../var/feeddb.sqlite3.db';
 
 }
